411f35d93251a0fec3e2d4d3cd98213f828d7953
Thomas Durieux <durieuxthomas@hotmail.com>
2014-02-21 01:58:34 +0100

    [add] Add http command

---
+++ src/java/lille1/car/asseman_durieux/paserelleFTP/PaserelleFTP.java
@@ -0,0 +1,98 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Thomas Durieux.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package lille1.car.asseman_durieux.paserelleFTP;
+
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Thomas Durieux
+ */
+public interface PaserelleFTP {
+
+  /**
+   * Download a file
+   *
+   * @param path the path to the file
+   *
+   * @return Response
+   */
+  Response getFile(String path);
+
+  /**
+   * Remove a file
+   *
+   * @param path the path to the file
+   *
+   * @return Response
+   */
+  Response removeFile(String path);
+
+  /**
+   * Store a file
+   *
+   * @param path the path to the file
+   *
+   * @return Response
+   */
+  Response storeFile(String path);
+
+  /**
+   * Get a representation of a Directory
+   *
+   * @param path   The path to the direcory
+   * @param format The output format (JSON, XML, HTML)
+   *
+   * @return
+   */
+  Response getDir(String path, String format);
+
+  /**
+   * Create a directory
+   *
+   * @param path The path to the direcory
+   *
+   * @return
+   */
+  Response mkDir(String path);
+
+  /**
+   * Remove a Directory
+   *
+   * @param path The path to the direcory
+   *
+   * @return
+   */
+  Response rmDir(String path);
+
+  /**
+   * Rename a resource
+   *
+   * @param fromPath the old path of the resource
+   * @param toPath   the new path of the resource
+   *
+   * @return
+   */
+  Response rename(String fromPath, String toPath);
+}
--- src/java/lille1/car/asseman_durieux/paserelleFTP/PaserelleFTPImpl.java
+++ src/java/lille1/car/asseman_durieux/paserelleFTP/PaserelleFTPImpl.java
@@ -1,14 +1,17 @@
 package lille1.car.asseman_durieux.paserelleFTP;
 
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import lille1.car.asseman_durieux.exception.AuthenticationException;
 
 /**
  * Access the FTP server through REST request
@@ -16,19 +19,96 @@ import javax.ws.rs.core.UriInfo;
  * @author Thomas Durieux
  */
 @Path("/")
-public class PaserelleFTPImpl {
+public class PaserelleFTPImpl implements PaserelleFTP {
 
   @Context
   HttpHeaders requestHeaders;
   @Context
   UriInfo uriInfo;
 
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
   @GET
   @Path("/file{path: .*}")
   @Produces(MediaType.APPLICATION_OCTET_STREAM)
   public Response getFile(@PathParam("path") String path) {
     ClientSession session = AuthenticationManager.INSTANCE.getSession(requestHeaders, uriInfo);
-    return Response.ok("").build();
+    return Response.ok(FTPCommand.INSTANCE.getFile(session, path)).build();
   }
-  
+
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
+  @GET
+  @Path("/dir{path: .*}/{format: (json|xml|html)}")
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  public Response getDir(@PathParam("path") String path, @PathParam("format") String format) {
+    ClientSession session;
+    try {
+      session = AuthenticationManager.INSTANCE.getSession(requestHeaders, uriInfo);
+    } catch (AuthenticationException e) {
+      return Response.ok("401 not allowed").status(401).header("WWW-Authenticate", "Basic").build();
+    }
+    if (format.equals("json")) {
+      return Response.ok(FTPCommand.INSTANCE.getDirectory(session, path).toJson()).header("Content-Type", "application/json").build();
+    } else if (format.equals("xml")) {
+      return Response.ok(FTPCommand.INSTANCE.getDirectory(session, path).toXML()).header("Content-Type", "application/xml").build();
+    } else if (format.equals("html")) {
+      return Response.ok(FTPCommand.INSTANCE.getDirectory(session, path).toHTML()).header("Content-Type", "text/html").build();
+    }
+    return Response.ok(FTPCommand.INSTANCE.getDirectory(session, path).toJson()).build();
+  }
+
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
+  @GET
+  @Path("/dir{path: .*}/{format: (json|xml|html)}")
+  public Response removeFile(@PathParam("path") String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
+  @PUT
+  @Path("/dir{path: .*}")
+  public Response storeFile(@PathParam("path") String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
+  @POST
+  @Path("/dir{path: .*}")
+  public Response mkDir(@PathParam("path") String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
+  @POST
+  @Path("/dir{path: .*}")
+  public Response rmDir(@PathParam("path") String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  /**
+   * @see PaserelleFTP
+   */
+  @Override
+  @POST
+  @Path("/dir{fromPath: .*}/{toPath: .*}")
+  public Response rename(@PathParam("fromPath") String fromPath, @PathParam("toPath") String toPath) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 }

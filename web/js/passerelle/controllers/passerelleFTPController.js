0df2e8389d43012c533efeb8bc73554153f15911
Thomas Durieux <durieuxthomas@hotmail.com>
2014-03-18 10:14:33 +0100

    [add] add interface to upload, download and create folder

--- web/js/passerelle/controllers/passerelleFTPController.js
+++ web/js/passerelle/controllers/passerelleFTPController.js
@@ -1,77 +1,136 @@
-angular.module("passerelleFTP.passerelleFTP.Controllers.passerelleFTPController",['passerelleFTP.passerelleFTP.Services.passerelleFTPService'])
-  .controller('dirsController', ['$rootScope', '$scope', '$log','MessagesService','passerelleFTPService',
-    function ($rootScope, $scope, $log,MessagesService,passerelleFTPService) {
-      $scope.dirs = [];
-      $scope.currentPath = "";
+angular.module("passerelleFTP.passerelleFTP.Controllers.passerelleFTPController", ['passerelleFTP.passerelleFTP.Services.passerelleFTPService'])
+        .controller('dirsController', ['$rootScope', '$scope', '$log', 'MessagesService', 'passerelleFTPService',
+          function($rootScope, $scope, $log, MessagesService, passerelleFTPService) {
+            $scope.dirs = [];
+            $scope.currentPath = "";
 
-      var loadDir = function (path) {
-        return passerelleFTPService.getDirs(path, $rootScope.loggedUser).then(function (data) {
-          $scope.current = data;
-          data.listFile.push({
-            creationDate: null,
-            group: null,
-            listFile: [],
-            name: "..",
-            owner: null,
-            size: null,
-            type: "directory"
-          });
-          $scope.search = "";
-          $scope.currentPath = data.name;
-        }, function (err) {
-          MessagesService.error(err);
-        });
-      };
-      function toBinaryString(data) {
-            var ret = [];
-            var len = data.length;
-            var byte;
-            for (var i = 0; i < len; i++) { 
-                byte=( data.charCodeAt(i) & 0xFF )>>> 0;
-                ret.push( String.fromCharCode(byte) );
+            var loadDir = function(path) {
+              return passerelleFTPService.getDirs(path, $rootScope.loggedUser).then(function(data) {
+                $scope.current = data;
+                data.listFile.push({
+                  creationDate: null,
+                  group: null,
+                  listFile: [],
+                  name: "..",
+                  owner: null,
+                  size: null,
+                  type: "directory"
+                });
+                $scope.search = "";
+                $scope.currentPath = data.name;
+              }, function(error) {
+                if (error.status == 401) {
+                  $rootScope.loggedUser = {};
+                  MessagesService.error("Non connecté");
+                } else {
+                  MessagesService.error(error);
+                }
+              });
+            };
+            function toBinaryString(data) {
+              var ret = [];
+              var len = data.length;
+              var byte;
+              for (var i = 0; i < len; i++) {
+                byte = (data.charCodeAt(i) & 0xFF) >>> 0;
+                ret.push(String.fromCharCode(byte));
+              }
+
+              return ret.join('');
             }
+            var downloadFile = function(path) {
 
-            return ret.join('');
-        }
-      var downloadFile = function(path) { 
-          
-          downloadURL(document.location.href.replace(document.location.hash, "")+"rest"+path);
-          return;
-        return passerelleFTPService.downloadFile(path, $rootScope.loggedUser).then(function(data) {
-            var data = toBinaryString(data.data);
-            data = "data:application/bytes;base64,"+btoa(data);
-            document.location = data;
-        });
-      };
-      
-      var downloadURL = function downloadURL(url) {
-            var hiddenIFrameID = 'hiddenDownloader',
-                iframe = document.getElementById(hiddenIFrameID);
-            if (iframe === null) {
+              downloadURL(document.location.href.replace(document.location.hash, "") + "rest" + path);
+              return;
+              return passerelleFTPService.downloadFile(path, $rootScope.loggedUser).then(function(data) {
+                var data = toBinaryString(data.data);
+                data = "data:application/bytes;base64," + btoa(data);
+                document.location = data;
+              });
+            };
+
+            var downloadURL = function downloadURL(url) {
+              var hiddenIFrameID = 'hiddenDownloader',
+                      iframe = document.getElementById(hiddenIFrameID);
+              if (iframe === null) {
                 iframe = document.createElement('iframe');
                 iframe.id = hiddenIFrameID;
                 iframe.style.display = 'none';
                 document.body.appendChild(iframe);
-            }
-            iframe.src = url;
-        };
+              }
+              iframe.src = url;
+            };
 
-      loadDir($scope.currentPath);
+            loadDir($scope.currentPath);
 
 
-      $scope.changeDir = function (dir) {
-        if(dir.type == "directory")
-            loadDir($scope.currentPath + "/" + dir.name + "/");
-        else
-            downloadFile($scope.currentPath + "/" + dir.name);
-      };
-      
-      $scope.delete = function(dir) {
-        return passerelleFTPService.delete($scope.currentPath + "/" + dir.name + "/",$rootScope.loggedUser).then(function(data) {
-            $scope.current.listFile.splice($scope.current.listFile.indexOf(dir),1);
-        }, function (error) {
-            alert(error);
-        });
-      }
-    }
-  ]);
+            $scope.changeDir = function(dir) {
+              if (dir.type == "directory")
+                loadDir($scope.currentPath + "/" + dir.name + "/");
+              else
+                downloadFile($scope.currentPath + "/" + dir.name);
+            };
+
+            $scope.delete = function(dir) {
+              return passerelleFTPService.delete($scope.currentPath + "/" + dir.name + "/", $rootScope.loggedUser).then(function(data) {
+                $scope.current.listFile.splice($scope.current.listFile.indexOf(dir), 1);
+              }, function(error) {
+                if (error.status == 401) {
+                  MessagesService.error("Non connecté");
+                  $rootScope.loggedUser = {};
+                } else {
+                  MessagesService.error(error);
+                }
+              });
+            };
+            $scope.fileChange = function(input) {
+              if (input.files.length > 0) {
+                $scope.file = input.files[0];
+              } else {
+                $scope.file = null;
+              }
+            };
+            $scope.upload = function() {
+              if (!$scope.file)
+                return;
+              return passerelleFTPService.uploadFile($scope.currentPath + "/" + $scope.file.name, $scope.file, $rootScope.loggedUser).then(function(data) {
+                $scope.file = null;
+                $scope.f = null;
+                loadDir($scope.currentPath);
+              }, function(error) {
+                if (error.status == 401) {
+                  MessagesService.error("Non connecté");
+                  $rootScope.loggedUser = {};
+                } else {
+                  MessagesService.error(error);
+                }
+              });
+            };
+            $scope.mkdir = function() {
+              var name = prompt("Nom du dossier");
+              return passerelleFTPService.mkdir($scope.currentPath + "/" + name, $rootScope.loggedUser).then(function(data) {
+                loadDir($scope.currentPath);
+              }, function(error) {
+                if (error.status == 401) {
+                  MessagesService.error("Non connecté");
+                  $rootScope.loggedUser = {};
+                } else {
+                  MessagesService.error(error);
+                }
+              });
+            };
+
+          }
+        ]).filter('bytesToSize', function() {
+  return function(bytes) {
+    var k = 1000;
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == null)
+      return '';
+
+    if (bytes == 0)
+      return '0 Bytes';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(k)), 10);
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+  };
+});
--- web/js/passerelle/services/passerelleFTPService.js
+++ web/js/passerelle/services/passerelleFTPService.js
@@ -1,48 +1,91 @@
 angular.module('passerelleFTP.passerelleFTP.Services.passerelleFTPService', ['base64'])
-  .factory("passerelleFTPService", ['$base64', '$http', '$q', '$log',
-    function ($base64, $http, $q, $log) {
-      var apiRootUrl = "rest";
+        .factory("passerelleFTPService", ['$base64', '$http', '$q', '$log',
+          function($base64, $http, $q, $log) {
+            var apiRootUrl = "rest";
 
-      return {
-        getDirs: function (path, user) {
-          if(path == null) path = "/";
-            path = path.replace(/\/$/, '')
-          if(path == "") path = "/";
-          var deferred = $q.defer();
-          var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
-          $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
-          $http.get(apiRootUrl + "/dir/"+path.replace(/\/$/, '')+"/json").then(function (data) {
-            if(data.status == 200) {
-              return deferred.resolve(data.data);
-            }
-            return deferred.reject(data);
-          });
-          return deferred.promise;
-        },
-        downloadFile: function (path, user) {
-          var deferred = $q.defer();
-          var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
-          $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
-          $http.get(apiRootUrl + "/"+path.replace(/\/$/, '')).then(function (data) {
-            if(data.status == 200) {
-              return deferred.resolve(data.data);
-            }
-            return deferred.reject(data);
-          });
-          return deferred.promise;
-        },
-        delete: function (path, user) {
-          var deferred = $q.defer();
-          var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
-          $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
-          $http.delete(apiRootUrl + "/"+path.replace(/\/$/, '')).then(function (data) {
-            if(data.status == 200) {
-              return deferred.resolve(data.data);
-            }
-            return deferred.reject(data);
-          });
-          return deferred.promise;
-        }
-      };
-    }
-  ]);
+            return {
+              getDirs: function(path, user) {
+                if (path == null)
+                  path = "/";
+                path = path.replace(/\/$/, '')
+                if (path == "")
+                  path = "/";
+                var deferred = $q.defer();
+                var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
+                $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                $http.get(apiRootUrl + "/dir/" + path.replace(/\/$/, '') + "/json").then(function(data) {
+                  if (data.status == 200) {
+                    return deferred.resolve(data.data);
+                  }
+                  return deferred.reject(data);
+                }, function(error) {
+                  return deferred.reject(error);
+                });
+                return deferred.promise;
+              },
+              downloadFile: function(path, user) {
+                var deferred = $q.defer();
+                var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
+                $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                $http.get(apiRootUrl + "/" + path.replace(/\/$/, '')).then(function(data) {
+                  if (data.status == 200) {
+                    return deferred.resolve(data.data);
+                  }
+                  return deferred.reject(data);
+                }, function(error) {
+                  return deferred.reject(error);
+                });
+                return deferred.promise;
+              },
+              uploadFile: function(path, file, user) {
+                var deferred = $q.defer();
+                var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
+                $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                var formData = new FormData();
+                formData.append('file', file);
+                $http({method: 'PUT', url: apiRootUrl + "/" + path.replace(/\/$/, ''), data: formData, headers: {'Content-Type': undefined}, transformRequest: angular.identity}).then(function(data) {
+                  if (data.status == 200) {
+                    return deferred.resolve(data.data);
+                  }
+                  return deferred.reject(data);
+                }, function(error) {
+                  return deferred.reject(error);
+                });
+                return deferred.promise;
+              },
+              delete: function(path, user) {
+                var deferred = $q.defer();
+                var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
+                $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                $http.delete(apiRootUrl + "/" + path.replace(/\/$/, '')).then(function(data) {
+                  if (data.status == 200) {
+                    return deferred.resolve(data.data);
+                  }
+                  return deferred.reject(data);
+                }, function(error) {
+                  return deferred.reject(error);
+                });
+                return deferred.promise;
+              },
+              mkdir: function(path, user) {
+                var deferred = $q.defer();
+                var encodedUserNameAndPassword = $base64.encode(user.username + ':' + user.password);
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
+                $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                $http.post(apiRootUrl + "/mkdir/" + path.replace(/\/$/, '')).then(function(data) {
+                  if (data.status == 200) {
+                    return deferred.resolve(data.data);
+                  }
+                  return deferred.reject(data);
+                }, function(error) {
+                  return deferred.reject(error);
+                });
+                return deferred.promise;
+              }
+            };
+          }
+        ]);

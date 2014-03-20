d72e578d5b53ca829b1490890d5ae9bc18d13a41
= <durieuxthomas@gmail.com>
2014-03-20 12:02:54 +0100

    correct authentification

--- test/lille1/car/asseman_durieux/paserelleFTP/PasserelleFTPTest.java
+++ test/lille1/car/asseman_durieux/paserelleFTP/PasserelleFTPTest.java
@@ -1,10 +1,9 @@
-package test.lille1.car.asseman_durieux.paserelleFTP;
+package lille1.car.asseman_durieux.paserelleFTP;
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,26 +16,26 @@ import com.jayway.restassured.*;
  * @author asseman
  */
 public class PasserelleFTPTest {
-    
+
     public PasserelleFTPTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     /**
      * Test authentification using the get command.
      */
@@ -47,18 +46,19 @@ public class PasserelleFTPTest {
                 when().
                 get("/PasserelleFTP/rest/dir///tmp/json");
     }
-    
+
     /**
      * Test FileNotFound return code using removeFile on a missing file.
      */
     @Test
     public void testFileNotFound() {
-        RestAssured.expect().
-                statusCode(404).
-                when().with().authentication().basic("user", "pass").
-                get("/PasserelleFTP/rest/tmp/azdlkugzadlugzeme5z7zelh");
+        RestAssured.expect().given()
+                .auth().preemptive()
+                .basic("user", "pass")
+                .when().get("/PasserelleFTP/rest/tmp/azdlkugzadlugzeme5z7zelh").then()
+                .statusCode(404);
+
     }
-    
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //

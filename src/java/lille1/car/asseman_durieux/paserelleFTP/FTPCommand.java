5c46afac228e5243a31d0f86e4f263b749258464
Thomas Durieux <durieuxthomas@hotmail.com>
2014-03-26 12:13:26 +0100

    add rmdir

--- src/java/lille1/car/asseman_durieux/paserelleFTP/FTPCommand.java
+++ src/java/lille1/car/asseman_durieux/paserelleFTP/FTPCommand.java
@@ -9,57 +9,75 @@ import lille1.car.asseman_durieux.paserelleFTP.resource.Directory;
  * @author Thomas Durieux
  */
 interface FTPCommand {
-    
-    /**
-     * Singloton
-     */
-    FTPCommand INSTANCE = new FTPCommandImpl();
 
-    /**
-     * Connect the client to the server
-     * @param clientSession the client 
-     */
-    void connectClient(ClientSession clientSession);
-    
-    /**
-     * Login the user to the server
-     * @param clientSession 
-     */
-    void loginClient(ClientSession clientSession);
-    
-    /**
-     * Disconnect the client 
-     * @param clientSession 
-     */
-    void disconnectClient(ClientSession clientSession);
+  /**
+   * Singloton
+   */
+  FTPCommand INSTANCE = new FTPCommandImpl();
 
-    /**
-     * Dowload a file
-     * @param clientSession the client witch download a file
-     * @param path the path to the document
-     * @return the input stream of the file
-     */
-    InputStream getFile(ClientSession clientSession, String path);
-    /**
-     * Upload a new file on the ftp server
-     * @param clientSession  the client witch upload a file
-     * @param path the path of the new file
-     * @param inputStream the inputStream of the file
-     */
-    void upload(ClientSession clientSession, String path, InputStream inputStream);
-   
-    /**
-     * Get the list of resource of a directory
-     * @param clientSession the client witch demand the list
-     * @param path the path of the directory
-     * @return the directory
-     */
-    Directory getDirectory(ClientSession clientSession, String path);
-    
-    /**
-     * Create a folder on the ftp server
-     * @param clientSession the client witch create the new folder
-     * @param path the path of the folder
-     */
-    public void mkdir(ClientSession clientSession, String path);
+  /**
+   * Connect the client to the server
+   *
+   * @param clientSession the client
+   */
+  void connectClient(ClientSession clientSession);
+
+  /**
+   * Login the user to the server
+   *
+   * @param clientSession
+   */
+  void loginClient(ClientSession clientSession);
+
+  /**
+   * Disconnect the client
+   *
+   * @param clientSession
+   */
+  void disconnectClient(ClientSession clientSession);
+
+  /**
+   * Dowload a file
+   *
+   * @param clientSession the client witch download a file
+   * @param path          the path to the document
+   *
+   * @return the input stream of the file
+   */
+  InputStream getFile(ClientSession clientSession, String path);
+
+  /**
+   * Upload a new file on the ftp server
+   *
+   * @param clientSession the client witch upload a file
+   * @param path          the path of the new file
+   * @param inputStream   the inputStream of the file
+   */
+  void upload(ClientSession clientSession, String path, InputStream inputStream);
+
+  /**
+   * Get the list of resource of a directory
+   *
+   * @param clientSession the client witch demand the list
+   * @param path          the path of the directory
+   *
+   * @return the directory
+   */
+  Directory getDirectory(ClientSession clientSession, String path);
+
+  /**
+   * Create a folder on the ftp server
+   *
+   * @param clientSession the client witch create the new folder
+   * @param path          the path of the folder
+   */
+  void mkdir(ClientSession clientSession, String path);
+
+  /**
+   * Remove a folder on the ftp server
+   *
+   * @param clientSession the client witch create the new folder
+   * @param path          the path of the folder
+   */
+  void rmdir(ClientSession session, String path);
 }
--- src/java/lille1/car/asseman_durieux/paserelleFTP/FTPCommandImpl.java
+++ src/java/lille1/car/asseman_durieux/paserelleFTP/FTPCommandImpl.java
@@ -19,160 +19,182 @@ import org.apache.commons.net.ftp.FTPFile;
  */
 public class FTPCommandImpl implements FTPCommand {
 
-    /**
-     * @see FTPCommand
-     */
-    public void connectClient(ClientSession clientSession) {
-        try {
-            String host = PasserelleFTPConfiguration.INSTANCE.getProperty("ftpHost");
-            int port = PasserelleFTPConfiguration.INSTANCE.getIntProperty("ftpPort");
-            clientSession.getFTPClient().connect(host, port);
-            clientSession.getFTPClient().enterLocalPassiveMode();
-        } catch (SocketException ex) {
-            throw new ClientSessionException("Unable to connect FTP client", ex);
-        } catch (IOException ex) {
-            throw new FTPCommandException("Unable to connect FTP client", ex);
-        }
+  /**
+   * @see FTPCommand
+   */
+  public void connectClient(ClientSession clientSession) {
+    try {
+      String host = PasserelleFTPConfiguration.INSTANCE.getProperty("ftpHost");
+      int port = PasserelleFTPConfiguration.INSTANCE.getIntProperty("ftpPort");
+      clientSession.getFTPClient().connect(host, port);
+      clientSession.getFTPClient().enterLocalPassiveMode();
+    } catch (SocketException ex) {
+      throw new ClientSessionException("Unable to connect FTP client", ex);
+    } catch (IOException ex) {
+      throw new FTPCommandException("Unable to connect FTP client", ex);
     }
+  }
 
-    /**
-     * @see FTPCommand
-     */
-    public void loginClient(ClientSession clientSession) {
-        try {
-            clientSession.connect();
-        } catch (FTPCommandException e) {
+  /**
+   * @see FTPCommand
+   */
+  public void loginClient(ClientSession clientSession) {
+    try {
+      clientSession.connect();
+    } catch (FTPCommandException e) {
+      clientSession.disconnect();
+      clientSession.connect();
+    }
+    try {
+      boolean value = clientSession.getFTPClient().login(clientSession.getUsername(), clientSession.getPassword());
+      if (!value) {
+        throw new FTPCommandException("Unable to login client: bad password");
+      }
+    } catch (IOException ex) {
+      throw new FTPCommandException("Unable to login client", ex);
+    }
+  }
+
+  /**
+   * @see FTPCommand
+   */
+  public void disconnectClient(ClientSession clientSession) {
+    try {
+      if (!clientSession.getFTPClient().logout()) {
+        throw new FTPCommandException("Unable to logout client");
+      }
+    } catch (IOException ex) {
+      /*
+       * On arrive dans ce cas lorsque le sereur FTP n'est pas accessible.
+       */
+    }
+  }
+
+  /**
+   * @see FTPCommand
+   */
+  public InputStream getFile(final ClientSession clientSession, String path) {
+    clientSession.login();
+    try {
+      InputStream inputStream = clientSession.getFTPClient().retrieveFileStream(path);
+      if (clientSession.getFTPClient().getReplyCode() != 150) {
+        throw new FTPCommandException("Unable to retreive file");
+      }
+      new Thread(new Runnable() {
+        public void run() {
+          try {
+            boolean tmp = clientSession.getFTPClient().completePendingCommand();
+          } catch (IOException ex) {
             clientSession.disconnect();
-            clientSession.connect();
+            throw new FTPCommandException("Unable to retreive file", ex);
+          }
         }
-        try {
-            boolean value = clientSession.getFTPClient().login(clientSession.getUsername(), clientSession.getPassword());
-            if (!value) {
-                throw new FTPCommandException("Unable to login client: bad password");
-            }
-        } catch (IOException ex) {
-            throw new FTPCommandException("Unable to login client", ex);
-        }
+      }).start();
+      return inputStream;
+    } catch (IOException e) {
+      clientSession.disconnect();
+      throw new FTPCommandException("Unable to retreive file", e);
     }
+  }
 
-    /**
-     * @see FTPCommand
-     */
-    public void disconnectClient(ClientSession clientSession) {
-        try {
-            if (!clientSession.getFTPClient().logout()) {
-                throw new FTPCommandException("Unable to logout client");
-            }
-        } catch (IOException ex) {
-            /*
-             * On arrive dans ce cas lorsque le sereur FTP n'est pas accessible.
-             */
-        }
+  /**
+   * @see FTPCommand
+   */
+  public static void deleteFile(ClientSession clientSession, String path) {
+    clientSession.login();
+    try {
+      int retCode = clientSession.getFTPClient().dele(path);
+      switch (retCode) {
+        case 250:
+          break;
+        case 550:
+          throw new FTPCommandException("Unable to delete file (no such file ?)");
+        default:
+          throw new FTPCommandException("Error during DELE command, return code : " + retCode);
+      }
+    } catch (IOException e) {
+      throw new FTPCommandException("IO error during deleting file");
     }
+  }
 
-    /**
-     * @see FTPCommand
-     */
-    public InputStream getFile(final ClientSession clientSession, String path) {
-        clientSession.login();
-        try {
-            InputStream inputStream = clientSession.getFTPClient().retrieveFileStream(path);
-            if(clientSession.getFTPClient().getReplyCode() != 150) {
-                throw new FTPCommandException("Unable to retreive file");
-            }
-            new Thread(new Runnable() {
-                public void run() {
-                    try {
-                        boolean tmp = clientSession.getFTPClient().completePendingCommand();
-                    } catch (IOException ex) {
-                        clientSession.disconnect();
-                        throw new FTPCommandException("Unable to retreive file", ex);
-                    }
-                }
-            }).start();
-            return inputStream;
-        } catch (IOException e) {
-            clientSession.disconnect();
-            throw new FTPCommandException("Unable to retreive file", e);
+  /**
+   * @see FTPCommand
+   */
+  public Directory getDirectory(ClientSession clientSession, String path) {
+    clientSession.login();
+    try {
+      clientSession.getFTPClient().setFileType(FTP.ASCII_FILE_TYPE);
+      int cwdValue = clientSession.getFTPClient().cwd(path);
+      if (cwdValue == 550) {
+        throw new FTPCommandException("Folder not found");
+      }
+      FTPFile[] ftpFiles = clientSession.getFTPClient().listFiles();
+      Directory directory = new DirectoryImp();
+      directory.setName(clientSession.getFTPClient().printWorkingDirectory());
+      for (FTPFile fTPFile : ftpFiles) {
+        Resource resource;
+        if (fTPFile.isDirectory()) {
+          resource = new DirectoryImp();
+        } else {
+          resource = new FileImp();
         }
+        resource.setName(fTPFile.getName());
+        resource.setOwner(fTPFile.getUser());
+        resource.setCreationDate(fTPFile.getTimestamp().getTime());
+        resource.setSize(fTPFile.getSize());
+        resource.setGroup(fTPFile.getGroup());
+        directory.addResource(resource);
+      }
+      return directory;
+    } catch (IOException e) {
+      clientSession.disconnect();
+      throw new FTPCommandException("Unable to retreive file", e);
     }
+  }
 
-    /**
-     * @see FTPCommand
-     */
-    public static void deleteFile(ClientSession clientSession, String path) {
-        clientSession.login();
-        try {
-            int retCode = clientSession.getFTPClient().dele(path);
-            switch (retCode) {
-                case 250:
-                    break;
-                case 550:
-                    throw new FTPCommandException("Unable to delete file (no such file ?)");
-                default:
-                    throw new FTPCommandException("Error during DELE command, return code : " + retCode);
-            }
-        } catch (IOException e) {
-            throw new FTPCommandException("IO error during deleting file");
-        }
+  /**
+   * @see FTPCommand
+   */
+  public void upload(ClientSession clientSession, String path, InputStream inputStream) {
+    clientSession.login();
+    try {
+      clientSession.getFTPClient().setFileType(FTP.BINARY_FILE_TYPE);
+      clientSession.getFTPClient().storeFile(path, inputStream);
+    } catch (IOException e) {
+      clientSession.disconnect();
+      throw new FTPCommandException("IO error during uploading file");
     }
+  }
 
-    /**
-     * @see FTPCommand
-     */
-    public Directory getDirectory(ClientSession clientSession, String path) {
-        clientSession.login();
-        try {
-            clientSession.getFTPClient().setFileType(FTP.ASCII_FILE_TYPE);
-            clientSession.getFTPClient().cwd(path);
-            FTPFile[] ftpFiles = clientSession.getFTPClient().listFiles();
-            Directory directory = new DirectoryImp();
-            directory.setName(clientSession.getFTPClient().printWorkingDirectory());
-            for (FTPFile fTPFile : ftpFiles) {
-                Resource resource;
-                if (fTPFile.isDirectory()) {
-                    resource = new DirectoryImp();
-                } else {
-                    resource = new FileImp();
-                }
-                resource.setName(fTPFile.getName());
-                resource.setOwner(fTPFile.getUser());
-                resource.setCreationDate(fTPFile.getTimestamp().getTime());
-                resource.setSize(fTPFile.getSize());
-                resource.setGroup(fTPFile.getGroup());
-                directory.addResource(resource);
-            }
-            return directory;
-        } catch (IOException e) {
-            clientSession.disconnect();
-            throw new FTPCommandException("Unable to retreive file", e);
-        }
+  /**
+   * @see FTPCommand
+   */
+  public void mkdir(ClientSession clientSession, String path) {
+    clientSession.login();
+    try {
+      int mkValue = clientSession.getFTPClient().mkd(path);
+      if (mkValue != 226) {
+        throw new FTPCommandException("Unable to create the directory");
+      }
+    } catch (IOException ex) {
+      clientSession.disconnect();
+      throw new FTPCommandException("Unable to create directory", ex);
     }
+  }
 
-    /**
-     * @see FTPCommand
-     */
-    public void upload(ClientSession clientSession, String path, InputStream inputStream) {
-        clientSession.login();
-        try {
-            clientSession.getFTPClient().setFileType(FTP.BINARY_FILE_TYPE);
-            clientSession.getFTPClient().storeFile(path, inputStream);
-        } catch (IOException e) {
-            clientSession.disconnect();
-            throw new FTPCommandException("IO error during uploading file");
-        }
+  /**
+   * @see FTPCommand
+   */
+  public void rmdir(ClientSession clientSession, String path) {
+    clientSession.login();
+    try {
+      int rmValue = clientSession.getFTPClient().rmd(path);
+      if (rmValue != 226) {
+        throw new FTPCommandException("Unable to remove the directory");
+      }
+    } catch (IOException ex) {
+      clientSession.disconnect();
+      throw new FTPCommandException("Unable to remove directory", ex);
     }
-
-    /**
-     * @see FTPCommand
-     */
-    public void mkdir(ClientSession clientSession, String path) {
-        clientSession.login();
-        try {
-            clientSession.getFTPClient().mkd(path);
-        } catch (IOException ex) {
-            clientSession.disconnect();
-            throw new FTPCommandException("Unable to create directory", ex);
-        }
-    }
+  }
 }

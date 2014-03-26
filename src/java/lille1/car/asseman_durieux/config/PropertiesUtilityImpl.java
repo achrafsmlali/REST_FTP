48be4beadbbaf0427f84396add373d90442a07a8
Thomas Durieux <durieuxthomas@hotmail.com>
2014-03-26 12:14:14 +0100

    remove log dependencies

--- src/java/lille1/car/asseman_durieux/config/PropertiesUtilityImpl.java
+++ src/java/lille1/car/asseman_durieux/config/PropertiesUtilityImpl.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import lille1.car.asseman_durieux.log.LoggerUtilities;
 
 /**
  * The Configuration class is a utility class used to load ini file
@@ -18,77 +17,77 @@ import lille1.car.asseman_durieux.log.LoggerUtilities;
  */
 public class PropertiesUtilityImpl implements PropertiesUtility {
 
-    /* Instance */
-    private final Properties properties;
+  /* Instance */
+  private final Properties properties;
 
-    /**
-     * Constructor
-     *
-     * @param fileName The property file name
-     */
-    public PropertiesUtilityImpl(final String fileName) {
-        /* Set instance */
-        properties = new Properties();
+  /**
+   * Constructor
+   *
+   * @param fileName The property file name
+   */
+  public PropertiesUtilityImpl(final String fileName) {
+    /* Set instance */
+    properties = new Properties();
 
-        /* Set file */
-        final File f = new File(fileName);
+    /* Set file */
+    final File f = new File(fileName);
 
-        /* Check */
-        if (!f.exists()) {
-            try {
-                properties.store(new FileOutputStream(f), null);
-            } catch (final IOException ex) {
-                Logger.getLogger(PropertiesUtilityImpl.class.getName()).log(
-                        Level.SEVERE, null, ex);
-            }
-        } else {
-            try {
-                // Load the properties file
-                properties.load(new FileInputStream(f));
-            } catch (final IOException ex) {
-                LoggerUtilities.error(ex);
-            }
-        }
+    /* Check */
+    if (!f.exists()) {
+      try {
+        properties.store(new FileOutputStream(f), null);
+      } catch (final IOException ex) {
+        Logger.getLogger(PropertiesUtilityImpl.class.getName()).log(
+                Level.SEVERE, null, ex);
+      }
+    } else {
+      try {
+        // Load the properties file
+        properties.load(new FileInputStream(f));
+      } catch (final IOException ex) {
+        throw new RuntimeException(ex);
+      }
     }
+  }
 
-    /**
-     * Constructor
-     *
-     * @param openStream The property file stream
-     */
-    public PropertiesUtilityImpl(final InputStream openStream) {
-        /* Set instance */
-        properties = new Properties();
+  /**
+   * Constructor
+   *
+   * @param openStream The property file stream
+   */
+  public PropertiesUtilityImpl(final InputStream openStream) {
+    /* Set instance */
+    properties = new Properties();
 
-        /* Use open stream */
-        try {
-            properties.load(openStream);
-        } catch (final IOException e) {
-            LoggerUtilities.error(e);
-        }
+    /* Use open stream */
+    try {
+      properties.load(openStream);
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    /**
-     * @see PropertiesUtility
-     */
-    @Override
-    public String getProperty(final String key) {
-        return properties.getProperty(key);
-    }
+  /**
+   * @see PropertiesUtility
+   */
+  @Override
+  public String getProperty(final String key) {
+    return properties.getProperty(key);
+  }
 
-    /**
-     * @see PropertiesUtility
-     */
-    @Override
-    public boolean getBooleanProperty(final String key) {
-        return Boolean.parseBoolean(properties.getProperty(key));
-    }
+  /**
+   * @see PropertiesUtility
+   */
+  @Override
+  public boolean getBooleanProperty(final String key) {
+    return Boolean.parseBoolean(properties.getProperty(key));
+  }
 
-    /**
-     * @see PropertiesUtility
-     */
-    @Override
-    public int getIntProperty(final String key) {
-        return Integer.parseInt(properties.getProperty(key));
-    }
+  /**
+   * @see PropertiesUtility
+   */
+  @Override
+  public int getIntProperty(final String key) {
+    return Integer.parseInt(properties.getProperty(key));
+  }
 }
